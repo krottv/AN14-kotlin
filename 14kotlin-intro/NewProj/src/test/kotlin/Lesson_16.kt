863b2b6e60146509e0959fce2ccd11890d3c845b
Alexandr Kalevich <98311174+AleksandrKalevich@users.noreply.github.com>
2022-03-23 17:50:20 +0300

    Lesson_16 Completed

---
+++ 14kotlin-intro/NewProj/src/test/kotlin/Lesson_16.kt
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Test
+
+data class Discount(val name: String, val percent: Double, val price: Int)
+
+class Lesson_16 {
+    @Test
+    fun test() {
+        val disc = listOf(Discount("d1", 13.1,4), Discount("d2", 10.5,55), Discount("d3", 43.1,65), Discount("d4", 65.0,70), Discount("d5", 16.3,45),
+            Discount("d6", 33.7,12), Discount("d7", 7.6,90), Discount("d8", 53.1,35), Discount("d9", 16.66,78), Discount("d10", 23.1,100))
+        println (disc.average { discount -> discount.price })
+    }
+    @Test
+    fun <T> List<T>.average(lambda: (T) -> Int): Int = this.sumOf(lambda) / this.size
+}
